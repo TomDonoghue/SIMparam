234c49db2cd759ea5a3271973c5beb6887618b02
TomDonoghue <tdonoghue@ucsd.edu>
2018-05-19 21:56:42 -0700

    Add violinplot

--- syn_plts.py
+++ syn_plts.py
@@ -3,6 +3,7 @@
 import numpy as np
 from scipy.stats import sem
 
+import seaborn as sns
 import matplotlib.pyplot as plt
 
 ###################################################################################################
@@ -29,6 +30,32 @@ def plot_errors(dat, title='Data', avg='mean', err='sem'):
     ax.set_title(title, fontsize=16)
     ax.set_xlabel('Noise Levels')
     ax.set_ylabel('Error')
+
+    # Set the top and right side frame & ticks off
+    ax.spines['right'].set_visible(False)
+    ax.spines['top'].set_visible(False)
+    ax.xaxis.set_ticks_position('bottom')
+    ax.yaxis.set_ticks_position('left')
+
+    # Set linewidth of remaining spines
+    ax.spines['left'].set_linewidth(2)
+    ax.spines['bottom'].set_linewidth(2)
+
+def plot_errors_violin(dat, title=None, y_label=None):
+
+    fig = plt.figure(figsize=[8, 6])
+
+    ax = sns.violinplot(data=dat.T, cut=0, scale='area', color='#2a60b7', saturation=0.75)#, bw=2)
+
+    plt.xticks([0, 1, 2, 3, 4],
+               [0.0, 0.025, 0.050, 0.075, 0.100]);
+
+    # Titles & Labels
+    ax.set_title(title, fontsize=16)
+    ax.set_xlabel('Noise Levels', fontsize=14)
+    if not y_label:
+        y_label = 'Error'
+    ax.set_ylabel(y_label, fontsize=14)
 
     # Set the top and right side frame & ticks off
     ax.spines['right'].set_visible(False)
