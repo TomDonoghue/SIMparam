15081a5e096a5663c14303222c067a83c973fc6f
TomDonoghue <tdonoghue@ucsd.edu>
2019-04-10 17:25:22 -0700

    Clean up / org aesthetics

--- syn_plts.py
+++ syn_plts.py
@@ -28,54 +28,38 @@ def plot_errors(dat, title='Data', avg='mean', err='sem'):
     ax.set_xlim([0.5, n_groups+0.5])
 
     # Titles & Labels
-    ax.set_title(title, fontsize=16)
+    ax.set_title(title)
     ax.set_xlabel('Noise Levels')
     ax.set_ylabel('Error')
 
-    # Set the top and right side frame & ticks off
-    ax.spines['right'].set_visible(False)
-    ax.spines['top'].set_visible(False)
-    ax.xaxis.set_ticks_position('bottom')
-    ax.yaxis.set_ticks_position('left')
+    # Set plot style
+    plot_style(ax)
 
-    # Set linewidth of remaining spines
-    ax.spines['left'].set_linewidth(2)
-    ax.spines['bottom'].set_linewidth(2)
 
 def plot_errors_violin(dat, title=None, x_axis='nlvs', y_label=None, save_fig=False, save_name=None):
     """Plots errors across distributions of fit data, as full distributions (as violin plot)."""
 
     fig = plt.figure(figsize=[8, 6])
 
-    ax = sns.violinplot(data=dat.T, cut=0, scale='area', color='#2a60b7', saturation=0.75)#, bw=2)
+    ax = sns.violinplot(data=dat.T, cut=0, scale='area', linewidth=2.5, color='#0c69ff', saturation=0.75)
 
-    # X-ticks & label for noise levels
+    # X-ticks & label for noise levels or # of peaks
     if x_axis == 'nlvs':
-        plt.xticks([0, 1, 2, 3, 4],
-                   [0.00, 0.025, 0.050, 0.100, 0.150]);
-        ax.set_xlabel('Noise Levels', fontsize=14)
-    # X-ticks & label for number of peaks
+        plt.xticks([0, 1, 2, 3, 4], [0.00, 0.025, 0.050, 0.100, 0.150]);
+        ax.set_xlabel('Noise Levels')
     if x_axis == 'n_oscs':
-        plt.xticks([0, 1, 2, 3, 4],
-                   [0, 1, 2, 3, 4]);
-        ax.set_xlabel('Number of Peaks', fontsize=14)
+        plt.xticks([0, 1, 2, 3, 4], [0, 1, 2, 3, 4]);
+        ax.set_xlabel('Number of Peaks')
 
     # Titles & Labels
     if title:
-        ax.set_title(title, fontsize=16)
+        ax.set_title(title)
     if not y_label:
         y_label = 'Error'
-    ax.set_ylabel(y_label, fontsize=14)
+    ax.set_ylabel(y_label)
 
-    # Set the top and right side frame & ticks off
-    ax.spines['right'].set_visible(False)
-    ax.spines['top'].set_visible(False)
-    ax.xaxis.set_ticks_position('bottom')
-    ax.yaxis.set_ticks_position('left')
-
-    # Set linewidth of remaining spines
-    ax.spines['left'].set_linewidth(2)
-    ax.spines['bottom'].set_linewidth(2)
+    ## Aesthetics
+    plot_style(ax)
 
     if save_fig:
 
@@ -93,9 +77,27 @@ def plot_n_oscs_bubbles(dat, save_fig=False):
     plt.xticks(list(range(0, 5)), list(range(0, 5)));
 
     # Titles & Labels
-    ax.set_title('Multiple Peak Fits', fontsize=16)
-    ax.set_xlabel('Number of Simulated Peaks', fontsize=14)
-    ax.set_ylabel('Number of Fit Peaks', fontsize=14)
+    ax.set_title('Multiple Peak Fits')
+    ax.set_xlabel('Number of Simulated Peaks')
+    ax.set_ylabel('Number of Fit Peaks')
+
+    # Set the plot style
+    plot_style(ax)
+
+    if save_fig:
+
+        save_name = 'plts/MultiplePeakFits.pdf'
+        plt.savefig(save_name, bbox_inches='tight', dpi=300)
+
+
+def plot_style(ax):
+    """Set the aesthetic styling for a plot."""
+
+    # Set word sizes
+    ax.title.set_size(20)
+    ax.xaxis.label.set_size(16)
+    ax.yaxis.label.set_size(16)
+    ax.tick_params(axis='both', which='major', labelsize=14)
 
     # Set the top and right side frame & ticks off
     ax.spines['right'].set_visible(False)
@@ -106,8 +108,3 @@ def plot_n_oscs_bubbles(dat, save_fig=False):
     # Set linewidth of remaining spines
     ax.spines['left'].set_linewidth(2)
     ax.spines['bottom'].set_linewidth(2)
-
-    if save_fig:
-
-        save_name = 'plts/MultiplePeakFits.pdf'
-        plt.savefig(save_name, bbox_inches='tight', dpi=300)
